f6586f1a7211f2311037b737d3b925accf955026
Gian Carlo <batagiancarlo@gmail.com>
2025-05-16 21:46:11 +0800

    Added schema.sql - It stores version metadata (to be manually changed when version changes).

---
+++ app/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS version;
+
+CREATE TABLE
+    version (
+        id INTEGER PRIMARY KEY,
+        version TEXT NOT NULL
+    );
+
+INSERT INTO version (id, version) VALUES (1, '1.0.0');
