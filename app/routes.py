54a96cd3c5b6c9a9bcbca377ab7fd6b028da0fb2
Gian Carlo <batagiancarlo@gmail.com>
2025-05-19 21:45:46 +0800

    Added an about and license page - Mira, design these pages

--- app/routes.py
+++ app/routes.py
@@ -11,3 +11,13 @@ bp = Blueprint('gizmo', __name__)
 @bp.route('/')
 def index():
     return render_template('routes/index.html')
+
+
+@bp.route('/license')
+def license():
+    return render_template('routes/license.html')
+
+
+@bp.route('/about')
+def about():
+    return render_template('routes/about.html')
